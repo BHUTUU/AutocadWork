d745ea681cf16f2a6141f9febbf6163e5a9417cb
BHUTUU <raj259942@gmail.com>
2024-03-24 19:43:58 +0530

    fixed some errors in the command line which is inteded for sending to the active modelspace of the autocad. Fixed a typo

--- ReplaceTextByMLeader.py
+++ ReplaceTextByMLeader.py
@@ -1,5 +1,5 @@
 import win32com.client
-
+ 
 class ReplaceTextByMLeader:
     @staticmethod
     def createMleader(coordinate_for_arrow, coordinate_for_lander,text_to_show_on_mleader):
@@ -9,7 +9,7 @@ class ReplaceTextByMLeader:
             return [False, "Autocad is not running. Run it and open the drawing as current in which you want me to work."]
         arrow_x, arrow_y = coordinate_for_arrow
         lander_x, lander_y = coordinate_for_lander
-        cmd = f'(command "MLEADER" "A" "{arrow_x},{arrow_y}" "L" "{lander_x},{lander_y}" "{text_to_show_on_mleader}) ""'
+        cmd=f'(command "MLEADER" "A" "{arrow_x},{arrow_y}" "L" "{lander_x},{lander_y}" "{text_to_show_on_mleader}") "" '
         try:
             acad.ActiveDocument.SendCommand(cmd)
             return [True, "Mleader created."]
@@ -30,7 +30,7 @@ class ReplaceTextByMLeader:
                 objectsFound += 1
                 if ifText:
                     if objects.objectName == "AcDbText":
-                        coordinates = (objects.InsetionPoint[0], objects.InsetionPoint[1])
+                        coordinates = (objects.InsertionPoint[0], objects.InsertionPoint[1])
                         TextValue = objects.TextString
                         objects.Delete()
                         coordinatesForLander = (float(coordinates[0])+10, float(coordinates[1])+10)
@@ -50,3 +50,5 @@ class ReplaceTextByMLeader:
                     return [False, "Both ifMtext and ifText is not True hence no task for me to do."]
         if objectsFound == 0:     
             return [True, "Task completed. But i didn't found any text or mtext in the proveded layer to replace."]
+# testInstance=ReplaceTextByMLeader()
+# testInstance.doReplace("new_layer")
