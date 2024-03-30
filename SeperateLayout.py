73af1e60048e9c76b21e5e8e6af328bbcde27a3a
BHUTUU <85620471+BHUTUU@users.noreply.github.com>
2024-03-31 02:22:37 +0530

    fixed wrong path handling method. now in working condition. still in i would say  not in final stage but can be used.

--- SeperateLayout.py
+++ SeperateLayout.py
@@ -1,4 +1,4 @@
-import win32com.client,os, shutil
+import win32com.client,os, shutil, time
 
 class SeperateLayouts:
     numberOfLayouts = []
@@ -9,20 +9,23 @@ class SeperateLayouts:
             acad = win32com.client.Dispatch("AutoCAD.Application")
             acad.Visible = True
             doc = acad.Documents.Open(path_to_source_dwg)
+            time.sleep(5)
             layouts = doc.Layouts
             cls.numberOfLayouts = len(layouts) - 1 #model space ko v count kr raha hai to remove kr diya 1.
             for layout in layouts:
                 layoutarray.append(layout.Name)
             layoutarray.remove("Model")
+            doc.Close()
             return (cls.numberOfLayouts, layoutarray)
         except Exception:
             return [False, "Could not connect to AutoCAD."]
     @classmethod
-    def deleteAllLayoutsExceptIndex(filePath, fileName: str, index: str):
+    def deleteAllLayoutsExceptIndex(cls,filePath, fileName: str, index: str):
         try:
             acad = win32com.client.Dispatch("AutoCAD.Application")
             acad.Visible = True
             doc = acad.Documents.Open(filePath)
+            time.sleep(2)
             layouts = doc.Layouts
             for layout in layouts:
                 layoutName = str(layout.Name)
@@ -30,14 +33,18 @@ class SeperateLayouts:
                     continue
                 else:
                     layout.Delete()
+                    doc.Save()
+            time.sleep(2)
+            layouts = doc.Layouts
             for layout in layouts:
                 layoutName = str(layout.Name)
-                if layoutName == "Model":
-                    continue
-                elif layoutName == fileName:
-                    break
-                else:
+                if layoutName != "Model":
                     layout.Name = fileName
+                    doc.save()
+            doc.Save()
+            time.sleep(2)
+            doc.Close()
+            return [True, "Deleted uneccesary layout tab"]
         except Exception:
             return [False, "Could not connect to AutoCAD."]
     @staticmethod
@@ -46,23 +53,31 @@ class SeperateLayouts:
             return [False, "The path to the source dwg is not valid."]
         if not os.path.isdir(path_to_dir_for_generated_dwg):
             return [False, "The path to the dir for generated dwg is not valid."]
+        print(path_to_dir_for_generated_dwg+"\n\n"+path_to_source_dwg)
         layoutNumberAndName = SeperateLayouts.getNumberOfLayouts(path_to_source_dwg)
+        print(layoutNumberAndName)
         if layoutNumberAndName[0] == False:
             return layoutNumberAndName
         numberOfLayouts = layoutNumberAndName[0]
-        # layoutarray = layoutNumberAndName[1]
-        baseNameOfDwg = str(path_to_source_dwg)[:-4]
+        layoutarray = layoutNumberAndName[1]
+        baseNameOfDwg = str(os.path.basename(path_to_source_dwg))[:-5]
+        print(numberOfLayouts, layoutarray, baseNameOfDwg)
         initialForRange=1
         errorInFiles=[]
-        if path_to_source_dwg in os.listdir(path_to_dir_for_generated_dwg):
+        if os.path.basename(path_to_source_dwg) in os.listdir(path_to_dir_for_generated_dwg):
             initialForRange=2
         for i in range(initialForRange, int(numberOfLayouts)+1):
             tempfilehold  = os.path.join(path_to_dir_for_generated_dwg, f"{baseNameOfDwg}{i}.dwg")
             if not os.path.isfile(tempfilehold):
                 shutil.copy(path_to_source_dwg, tempfilehold)
-                delResponse = SeperateLayouts.deleteAllLayoutsExceptIndex(tempfilehold, f"{baseNameOfDwg}{i}.dwg", str(i))
+                delResponse = SeperateLayouts.deleteAllLayoutsExceptIndex(str(tempfilehold), f"{baseNameOfDwg}{i}", str(i))
                 if delResponse[0] == False:
                     errorInFiles.append(tempfilehold+": error while deleting layouts for this file")
             else:
                 errorInFiles.append(tempfilehold+": This file already exists at destination so i will not even delete layouts from it.")
         return [True, errorInFiles]
+ins=SeperateLayouts()
+fileName = os.path.join(os.getcwd(), "test1.dwg")
+folderName = os.path.join(os.getcwd(), "result") 
+resp=ins.doSeparate(fileName, folderName)
+print(resp)
